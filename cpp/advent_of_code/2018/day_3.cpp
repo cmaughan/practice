610849f4e04037cf9c9179c7a446635f78ac45df
Chris Maughan <cmaughan@snarlsoftware.com>
2018-12-03 21:09:13 +0000

    More use of auto

--- cpp/advent_of_code/2018/day_3.cpp
+++ cpp/advent_of_code/2018/day_3.cpp
@@ -15,6 +15,7 @@ REGISTER_PROBLEM(AOC_2018_Day3)
         glm::ivec2 size;
     };
 
+    Timer t("Parse");
     std::vector<cut> cuts;
     glm::ivec2 max_rect(0);
     std::istringstream iss(file_read(PRACTICE_ROOT "/advent_of_code/2018/inputs/day_3.txt"));
@@ -28,6 +29,7 @@ REGISTER_PROBLEM(AOC_2018_Day3)
         max_rect.x = std::max(max_rect.x, (c.coord + c.size).x);
         max_rect.y = std::max(max_rect.y, (c.coord + c.size).y);
     }
+    t.Stop();
 
     // Make the cloth
     std::vector<int> cloth(max_rect.x * max_rect.y, 0);
@@ -36,9 +38,9 @@ REGISTER_PROBLEM(AOC_2018_Day3)
     auto walk_cloth_piece = [&](auto coord, auto size, auto fn)
     {
         auto bottom_right = coord + size;
-        for (int x = coord.x; x < bottom_right.x; x++)
+        for (auto x = coord.x; x < bottom_right.x; x++)
         {
-            for (int y = coord.y; y < bottom_right.y; y++)
+            for (auto y = coord.y; y < bottom_right.y; y++)
             {
                 fn(cloth[x + y * max_rect.x]);
             }
@@ -48,17 +50,17 @@ REGISTER_PROBLEM(AOC_2018_Day3)
     // Fill the areas
     for (auto& c : cuts)
     {
-        walk_cloth_piece(c.coord, c.size, [](int& val) { val++; });
+        walk_cloth_piece(c.coord, c.size, [](auto& val) { val++; });
     }
 
     // count the covered squares
-    LOG(INFO) << "Part 1: " << std::count_if(cloth.begin(), cloth.end(), [](int val) { return val > 1; });
+    LOG(INFO) << "Part 1: " << std::count_if(cloth.begin(), cloth.end(), [](auto val) { return val > 1; });
 
     // Part 2, find the piece on its own
     for (auto& c : cuts)
     {
-        bool found = true;
-        walk_cloth_piece(c.coord, c.size, [&found](int& val)
+        auto found = true;
+        walk_cloth_piece(c.coord, c.size, [&found](auto& val)
         {
             if (val > 1)
                 found = false;
