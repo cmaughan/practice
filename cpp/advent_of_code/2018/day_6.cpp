feb69b74be206e3c99ff77de4184833e494eca78
Chris Maughan <cmaughan@snarlsoftware.com>
2018-12-06 09:45:15 +0000

    Day 6

---
+++ cpp/advent_of_code/2018/day_6.cpp
@@ -0,0 +1,138 @@
+#include "common.h"
+
+#include "fileutils.h"
+#include "stringutils.h"
+#include "utils.h"
+#include <glm/glm.hpp>
+#include <algorithm>
+
+using namespace std;
+
+auto sample_input = std::string(R"(1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9)");
+
+REGISTER_PROBLEM(AOC_2018_Day6)
+{
+    std::vector<glm::ivec2> coords;
+    glm::ivec2 size(0);
+    auto input = utils_get_integer_grid(file_read(PRACTICE_ROOT "/advent_of_code/2018/inputs/day_6.txt"));
+    for (auto& row : input)
+    {
+        coords.push_back(glm::ivec2(row[0], row[1]));
+        size.x = std::max(size.x, row[0] + 1);
+        size.y = std::max(size.y, row[1] + 1);
+    }
+
+    struct Cell { int distance = std::numeric_limits<int>::max(); int owner = -1; };
+    std::vector<Cell> cells(size.x * size.y);
+
+    auto get_cell = [&](int x, int y) -> Cell& { return cells[y * size.x + x]; };
+    for (int owner = 0; owner < coords.size(); owner++)
+    {
+        auto coord = coords[owner];
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                auto& cell = get_cell(x, y);
+                int manhatten = std::abs(coord.x - x) + std::abs(coord.y - y);
+                if (manhatten < cell.distance)
+                {
+                    cell.distance = manhatten;
+                    cell.owner = owner;
+                }
+                if (manhatten == cell.distance && cell.owner != owner)
+                {
+                    cell.owner = -1;
+                }
+            }
+        }
+    }
+
+    std::ostringstream str;
+    for (int y = 0; y < size.y; y++)
+    {
+        for (int x = 0; x < size.x; x++)
+        {
+            auto cell = get_cell(x, y);
+            if (cell.owner == -1)
+            {
+                str << ".";
+            }
+            else
+            {
+                str << (char)(cell.owner + 'A');
+            }
+        }
+        str << std::endl;
+    }
+    std::cout << str.str();
+    //file_write("out.txt", str.str().c_str(), str.str().length());
+
+    std::map<int, int> owner_counts;
+    for (int i = 0; i < coords.size(); i++)
+    {
+        owner_counts[i] = 0;
+    }
+    for (int x = 0; x < size.x; x++)
+    {
+        for (int y = 0; y < size.y; y++)
+        {
+            auto& cell = get_cell(x, y);
+            if (owner_counts.find(cell.owner) != owner_counts.end())
+            {
+                if (x == 0 || y == 0 || x == (size.x - 1) || y == (size.y - 1))
+                {
+                    if (cell.owner != -1)
+                    {
+                        owner_counts.erase(cell.owner);
+                    }
+                }
+                else
+                {
+                    owner_counts[cell.owner]++;
+                }
+            }
+        }
+    }
+
+    int max = 0;
+    int id = -1;
+    for (auto& m : owner_counts)
+    {
+        if (m.second > max)
+        {
+            max = m.second;
+            id = m.first;
+        }
+    }
+
+    LOG(INFO) << "Part 1: " << max;
+
+    int under = 0;
+    for (int x = -10000 - size.x; x < 10000 + size.x; x++)
+    {
+        for (int y = -10000; y < size.y + 10000; y++)
+        {
+            int total = 0;
+            for (int owner = 0; owner < coords.size(); owner++)
+            {
+                auto coord = coords[owner];
+                total += std::abs(coord.x - x) + std::abs(coord.y - y);
+                if (total >= 10000)
+                    break;
+            }
+            if (total < 10000)
+            {
+                under++;
+            }
+        }
+    }
+
+    LOG(INFO) << "Part 2: " << under;
+}
+
