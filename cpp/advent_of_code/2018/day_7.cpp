76c73d46b7af50871dfb375cc370c5d2d08f543a
Chris Maughan <cmaughan@snarlsoftware.com>
2018-12-07 16:09:05 +0000

    Cleanup

--- cpp/advent_of_code/2018/day_7.cpp
+++ cpp/advent_of_code/2018/day_7.cpp
@@ -18,7 +18,7 @@ Step F must be finished before step E can begin)");
 
 REGISTER_PROBLEM(AOC_2018_Day7)
 {
-    std::map<char, std::set<char>> pre_req;
+    std::map<char, std::set<char>> pre_required;
     std::istringstream iss(file_read(PRACTICE_ROOT "/advent_of_code/2018/inputs/day_7.txt"));
     std::vector<string> lines;
     for (std::string line; std::getline(iss, line); )
@@ -27,7 +27,7 @@ REGISTER_PROBLEM(AOC_2018_Day7)
         char finished[2];
         char next[2];
         sscanf(line.c_str(), "%s %s %s %s %s %s %s %s", word, finished, word, word, word, word, word, next);
-        pre_req[next[0]].insert(finished[0]);
+        pre_required[next[0]].insert(finished[0]);
     }
 
     // Walk the pre required, and store all other pre-required
@@ -36,14 +36,14 @@ REGISTER_PROBLEM(AOC_2018_Day7)
     do
     {
         added = false;
-        for (auto& pre_char : pre_req)
+        for (auto& pre_char : pre_required)
         {
             for (;;)
             {
                 std::set<char> keys = pre_char.second;
                 for (auto& key : keys)
                 {
-                    pre_char.second.insert(pre_req[key].begin(), pre_req[key].end());
+                    pre_char.second.insert(pre_required[key].begin(), pre_required[key].end());
                 }
                 if (keys.size() != pre_char.second.size())
                 {
@@ -55,109 +55,91 @@ REGISTER_PROBLEM(AOC_2018_Day7)
         }
     } while (added);
 
-    // Copy for later
-    std::map<char, std::set<char>> pre_req2 = pre_req;
-
-    // Remove this character from the pre-required values
-    auto remove_requirement = [&](char c)
+    auto walk = [&](int numWorkers, std::map<char, std::set<char>> pre_req)
+        -> std::pair<std::string, int>
     {
-        for (auto& r : pre_req)
-        {
-            r.second.erase(c);
-        }
-        pre_req.erase(c);
-    };
+        int time = 0;
+        std::ostringstream str;
 
-    // Find the minimum required character each time and display it.
-    std::ostringstream str;
-    do
-    {
-        auto current = pre_req.begin();
-        while (current != pre_req.end())
+        // Remove this character from the pre-required values
+        auto remove_requirement = [&](char c)
         {
-            // Find the first char who's requirements are satisfied
-            if (pre_req[current->first].empty())
+            for (auto& r : pre_req)
             {
-                str << current->first;
-                remove_requirement(current->first);
-                current = pre_req.begin();
+                r.second.erase(c);
             }
-            else
-            {
-                current++;
-            }
-        }
-    } while (!pre_req.empty());
-    LOG(INFO) << "Part1 : " << str.str();
-
-    // Part 2
-    // A worker 'thread'
-    struct worker
-    {
-        char c = 0;
-        int remaining = 0;
-    };
-    std::vector<worker> workers(5);
-
-    // Next empty worker in the pool
-    auto find_empty_worker = [&]() -> worker* 
-    {
-        for (auto& w : workers)
+            pre_req.erase(c);
+        };
+    
+        // A worker 'thread'
+        struct worker
         {
-            if (w.remaining == 0)
-                return &w;
-        }
-        return nullptr;
-    };
+            char c = 0;
+            int remaining = 0;
+        };
+        std::vector<worker> workers(numWorkers);
 
-    // Tick the workers
-    auto do_work = [&]()
-    {
-        bool did_work = false;
-        for (auto& w : workers)
+        // Next empty worker in the pool
+        auto find_empty_worker = [&]() -> worker*
         {
-            if (w.remaining > 0)
+            for (auto& w : workers)
             {
-                w.remaining--;
                 if (w.remaining == 0)
-                {
-                    // Worker done, remove the work packet
-                    remove_requirement(w.c);
-                    w.c = 0;
-                }
-                did_work = true;
+                    return &w;
             }
-        }
-        return did_work;
+            return nullptr;
+        };
+
+        // Tick the workers
+        auto do_work = [&]()
+        {
+            bool did_work = false;
+            for (auto& w : workers)
+            {
+                if (w.remaining > 0)
+                {
+                    w.remaining--;
+                    if (w.remaining == 0)
+                    {
+                        // Worker done, remove the work packet
+                        remove_requirement(w.c);
+                        w.c = 0;
+                    }
+                    did_work = true;
+                }
+            }
+            return did_work;
+        };
+
+        std::set<char> assigned;
+        do
+        {
+            // Schedule work
+            for (auto& p : pre_req)
+            {
+                if (pre_req[p.first].empty() && assigned.find(p.first) == assigned.end())
+                {
+                    auto w = find_empty_worker();
+                    if (w)
+                    {
+                        w->c = p.first;
+                        w->remaining = w->c - 'A' + 1 + 60;
+                        assigned.insert(w->c);
+                        str << w->c;
+                    }
+                }
+            }
+
+            // Do work
+            time++;
+            do_work();
+        } while (!pre_req.empty());
+        return { str.str(), time };
     };
 
-    // Part 2
-    std::ostringstream str2;
-    int time = 0;
-    pre_req = pre_req2;
-    std::set<char> assigned;
+    auto[str1, time1] = walk(1, pre_required);
+    LOG(INFO) << "Part1: " << str1;
 
-    do
-    {
-        // Schedule work
-        for (auto& p : pre_req)
-        {
-            if (pre_req[p.first].empty() && assigned.find(p.first) == assigned.end())
-            {
-                auto w = find_empty_worker();
-                if (w)
-                {
-                    w->c = p.first;
-                    w->remaining = w->c - 'A' + 1 + 60;
-                    assigned.insert(w->c);
-                }
-            }
-        }
-
-        // Do work
-        time++;
-        do_work();
-    } while (!pre_req.empty());
-
-    LOG(INFO) << "Part2: " << time;
+    auto[str2, time2] = walk(5, pre_required);
+    LOG(INFO) << "Part2: " << time2;
 }
