5b71289dbb748031c762546a4c846553358d6b23
Chris Maughan <cmaughan@snarlsoftware.com>
2018-12-07 16:18:30 +0000

    Cleanup

--- cpp/advent_of_code/2018/day_7.cpp
+++ cpp/advent_of_code/2018/day_7.cpp
@@ -8,13 +8,6 @@
 #include <deque>
 
 using namespace std;
-string test_input(R"(Step C must be finished before step A can begin.
-Step C must be finished before step F can begin.
-Step A must be finished before step B can begin.
-Step A must be finished before step D can begin.
-Step B must be finished before step E can begin.
-Step D must be finished before step E can begin.
-Step F must be finished before step E can begin)");
 
 REGISTER_PROBLEM(AOC_2018_Day7)
 {
