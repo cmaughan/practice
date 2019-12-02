d94f6d1bec1ffdbe29cf171ca084139d523663bf
Chris Maughan <cmaughan@snarlsoftware.com>
2019-12-02 21:40:04 +0000

    Day2

--- cpp/advent_of_code/2019/day_2.cpp
+++ cpp/advent_of_code/2019/day_2.cpp
@@ -19,31 +19,38 @@ REGISTER_PROBLEM(AOC_2019_Day2)
     ops[1] = [&](auto& mem, int& pc) { mem[mem[pc + 3]] = mem[mem[pc + 1]] + mem[mem[pc + 2]]; pc += 4; };
     ops[2] = [&](auto& mem, int& pc) { mem[mem[pc + 3]] = mem[mem[pc + 1]] * mem[mem[pc + 2]]; pc += 4; };
 
+    auto testNounVerb = [&](int noun, int verb) {
+        std::vector<int> mem = in;
+
+        mem[1] = noun;
+        mem[2] = verb;
+
+        int pc = 0;
+        while (pc < mem.size())
+        {
+            if (mem[pc] == 99)
+                break;
+
+            auto itrCode = ops.find(mem[pc]);
+            if (itrCode != ops.end())
+            {
+                itrCode->second(mem, pc);
+            }
+        }
+        return mem[0];
+    };
+
+    LOG(INFO) << "Day1: " << testNounVerb(12, 2);
+
     // Search
     for (int noun = 0; noun <= 99; noun++)
     {
         for (int verb = 0; verb <= 99; verb++)
         {
-            std::vector<int> mem = in;
-
-            mem[1] = noun;
-            mem[2] = verb;
-
-            int pc = 0;
-            while (pc < mem.size())
-            {
-                if (mem[pc] == 99)
-                    break;
-                
-                auto itrCode = ops.find(mem[pc]);
-                if (itrCode != ops.end())
-                {
-                    itrCode->second(mem, pc);
-                }
-            }
-            if (mem[0] == 19690720)
-            {
+            if (testNounVerb(noun, verb) == 19690720)
+            { 
                 LOG(INFO) << "Day2: " << (100 * noun + verb);
+                break;
             }
         }
     }
