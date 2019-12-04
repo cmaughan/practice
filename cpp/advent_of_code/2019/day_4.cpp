7087ca858fe3903b0aa2a511a92bed2ab4c7e7c5
Chris Maughan <cmaughan@snarlsoftware.com>
2019-12-04 08:18:20 +0000

    Day4

--- cpp/advent_of_code/2019/day_4.cpp
+++ cpp/advent_of_code/2019/day_4.cpp
@@ -17,8 +17,41 @@ using namespace MUtils;
 
 REGISTER_PROBLEM(AOC_2019_Day4)
 {
-    auto in = string_get_string_grid(file_read(PRACTICE_ROOT "/advent_of_code/2019/inputs/day_4.txt"));
+    uint64_t low = 246515;
+    uint64_t high = 739105;
 
-    //LOG(INFO) << "Part2 :" << minSteps;
+    uint32_t count = 0;
+    for (auto current = low; current <= high; current++)
+    {
+        auto str = std::to_string(current);
+        int last = 0;
+        int i = 0;
+        int doubles = 0;
+        bool foundPair = false;
+        for (i = 0; i < str.length(); i++)
+        {
+            if (str[i] < last)
+                break;
+            if (last == str[i])
+            {
+                doubles++;
+            }
+            else if (doubles == 1)
+            {
+                foundPair = true;
+                doubles = 0;
+            }
+            last = str[i];
+        }
+
+        if (doubles == 1)
+            foundPair = true;
+
+        if (i < str.length() || !foundPair)
+            continue;
+
+        count++;
+    }
+    LOG(INFO) << "Day2: " << count;
 }
 
