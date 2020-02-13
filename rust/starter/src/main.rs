610b1293c0405edb66c9e5ea08e598c45499b212
Chris Maughan <cmaughan@snarlsoftware.com>
2020-02-13 08:15:32 +0000

    update rust practice project

--- rust/starter/src/main.rs
+++ rust/starter/src/main.rs
@@ -1,11 +1,13 @@
 use std::io;
-use rand::Rng;
+use rand::RngCore;
 
 fn main() {
     loop {
         println!("Guess a number:");
         let mut guess = String::new();
 
+        let val = rand::thread_rng().next_u32();
+        println!("Val {}", val);
         io::stdin().read_line(&mut guess)
             .expect("Bad");
 
