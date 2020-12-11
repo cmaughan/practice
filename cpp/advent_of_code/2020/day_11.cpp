8ef8866ff03b8c4c59ac4588c4fb6402f1ea8793
cmaughan <mornymorny@gmail.com>
2020-12-11 08:15:42 +0000

    Day11

---
+++ cpp/advent_of_code/2020/day_11.cpp
@@ -0,0 +1,65 @@
+#include "common.h"
+
+#include <algorithm>
+#include <fstream>
+#include <numeric>
+#include <regex>
+#include <set>
+#include <mutils/time/profiler.h>
+#include <mutils/math/math.h>
+#include <mutils/logger/logger.h>
+#include <mutils/string/string_utils.h>
+
+#include <imgui.h>
+#include <implot.h>
+
+#include <map>
+#include <set>
+#include <fmt/format.h>
+
+using namespace std;
+using namespace MUtils;
+
+namespace
+{
+    std::string part1;
+    std::string part2;
+
+    class Once
+    {
+    public:
+        Once(std::function<void()> fn)
+        {
+            fn();
+        }
+    };
+};
+
+#define BEGIN_ONCE static Once once([]()
+#define END_ONCE ); 
+
+REGISTER_PROBLEM(AOC_2020_Day11)
+{
+    PROFILE_SCOPE(Solution);
+
+    std::vector<std::string> lines;
+    auto in = string_get_integers(file_read(PRACTICE_ROOT "/advent_of_code/2020/inputs/day_11.txt"));
+
+
+    PROFILE_SCOPE(ResultPlot);
+    if (ImGui::Begin("Results"))
+    {
+        ImGui::Text(fmt::format("Part1: {}", part1).c_str());
+        ImGui::Text(fmt::format("Part2: {}", part2).c_str());
+    }
+    ImGui::End();
+
+    static int resetCount = 2;
+    if (resetCount == 0)
+    {
+        Profiler::SetPaused(true);
+    }
+    resetCount--;
+}
+
+
