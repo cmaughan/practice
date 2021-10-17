b2fecaeff961dbeb97e215799ba54c83a399e980
cmaughan <mornymorny@gmail.com>
2021-10-17 17:35:06 +0100

    csharp

---
+++ csharp/aoc_2020/day1.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Y2020.Day01 {
+
+    [ProblemName("Report Repair")]
+    class Solution : Solver {
+
+        public object PartOne(string input) {
+            var numbers = Numbers(input);
+            return (
+                from x in numbers 
+                let y = 2020 - x
+                where numbers.Contains(y)
+                select x * y
+            ).First();
+        }
+
+        public object PartTwo(string input) {
+            var numbers = Numbers(input);
+            return (
+                from x in numbers 
+                from y in numbers 
+                let z = 2020 - x - y
+                where numbers.Contains(z)
+                select x * y * z
+            ).First();
+        }
+
+        HashSet<int> Numbers(string input) {
+            return input.Split('\n').Select(int.Parse).ToHashSet<int>();
+        }
+    }
+}
