295775bb014e097c41f5a2b9f71cd574dc8beb02
Chris Maughan <mornymorny@gmail.com>
2021-12-01 18:37:46 +0000

    Advent of code

--- rust/advent_of_code/aoc2021/day1/src/main.rs
+++ rust/advent_of_code/aoc2021/day1/src/main.rs
@@ -1,16 +1,34 @@
 use itertools::Itertools;
+use itermore::IterMore;
 
-fn part1and2(combinations : usize) -> usize {
+fn part1() -> usize {
     include_str!("../input.txt")
-        .lines()
-        .map(|i| i.parse::<usize>().unwrap())
-        .combinations(combinations)
-        .filter(|a| a.iter().sum::<usize>() == 2020)
-        .map(|a| a.iter().product())
-        .next().unwrap()
+        .split_ascii_whitespace()
+        .map(|i| i.parse::<i32>().unwrap())
+        .windows()
+        .filter(|[a,b]| (a < b))
+        .count()
+}
+
+fn part2() -> usize {
+    include_str!("../input.txt")
+        .split_ascii_whitespace()
+        .map(|i| i.parse::<i32>().unwrap())
+        .tuple_windows()
+        .filter(|(first, _, _, last)| first < last)
+        .count()
+}
+
+fn part1and2<const N: usize>() -> usize {
+    include_str!("../input.txt")
+        .split_ascii_whitespace()
+        .map(|i| i.parse().unwrap())
+        .windows()
+        .filter(|a:&[i32;N]| (a.last() > a.first()))
+        .count()
 }
 
 pub fn main() {
-    println!("day1_p1 {}", part1and2(2));
-    println!("day1_p2 {}", part1and2(3));
+    println!("day1_p1 {}", part1and2::<2>());
+    println!("day1_p2 {}", part1and2::<4>());
 }
