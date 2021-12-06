f3afa62a5eb3f6f33df69dc71e798d1330abe66b
Chris Maughan <mornymorny@gmail.com>
2021-12-06 15:32:39 +0000

    5

--- rust/advent_of_code/aoc2021/day4/src/main.rs
+++ rust/advent_of_code/aoc2021/day4/src/main.rs
@@ -1,37 +1,91 @@
 use std::collections::hash_set::HashSet;
-use itermore::IterMore;
 
-fn part1() -> u32 {
-    3
+const SIZE: usize = 5;
+
+struct Board {
+    numbers: Vec<u32>,
+    found: HashSet<u32>,
+    won: bool
 }
 
-fn part2() -> u32 {
-    3
-}
-#[derive(Debug)]
-struct board {
-   numbers: HashSet<u32>,
-   rows: [u32; 5],
-   columns: [HashSet<u32>; 5]
-}
-impl board {
-    fn new(vals: Vec<u32>) -> board {
-        let mut h = HashSet::new();
-        let mut rows = [HashSet<u32>;5];
-        let mut columns: [HashSet<u32>;5];
-        for v in vals {
-           h.insert(v) ;
-        }
-
-        board {
-            numbers: h
+impl Board {
+    fn new(vals: Vec<u32>) -> Board {
+        Board {
+            numbers: vals,
+            found: HashSet::new(),
+            won: false
         }
     }
+
+    fn at(&self, x: usize, y: usize) -> u32 {
+        return self.numbers[y * SIZE + x];
+    }
+
+    fn score(&self) -> u32 {
+        self.numbers.iter().filter(|n| {
+            !self.found.contains(n)
+        }).sum::<u32>()
+
+    }
+    pub fn add_number(&mut self, num : u32) {
+        for x in 0..SIZE {
+            for y in 0..SIZE {
+                if num == self.at(x, y) {
+                    self.found.insert(num);
+                }
+            }
+        }
+    }
+    fn check_winner(&mut self) -> u32 {
+        for major in 0..SIZE {
+            if (0..SIZE).all(|x| self.found.contains(&self.at(x, major))) {
+                return self.score();
+            }
+            if (0..SIZE).all(|y| self.found.contains(&self.at(major, y))) {
+                return self.score();
+            }
+        }
+        return 0
+    }
 }
+
+fn part1(boards: &mut Vec<Board>, vals: &[u32]) -> u32 {
+    for num in vals {
+        for board in boards.iter_mut() {
+            board.add_number(*num);
+            let val = board.check_winner();
+            if val != 0 {
+                board.won = true;
+                return val * *num;
+            }
+        }
+    }
+    return 0
+}
+
+fn part2(boards: &mut Vec<Board>, vals: &[u32]) -> u32 {
+    let mut last_score = 0;
+    for num in vals {
+        for board in boards.iter_mut() {
+            if !board.won {
+                board.add_number(*num);
+                let val = board.check_winner();
+                if val != 0 {
+                    last_score = val * *num;
+                    board.won = true;
+                }
+            }
+        }
+    }
+    last_score
+}
+
 pub fn main() {
     let input = include_str!("../input.txt");
     let nums: Vec<u32> = input.lines().next().unwrap().split(',').map(|v| { v.parse().unwrap() })
         .collect();
+
+    // Called numbers
     let mut vals = Vec::new();
     for l in input.lines().skip(1) {
         for v in l.split_ascii_whitespace() {
@@ -39,11 +93,12 @@ pub fn main() {
         }
     }
 
+    // Boards
     let mut boards = Vec::new();
     for chunk in vals.chunks(25) {
-        boards.push(board::new(chunk.to_vec()));
+        boards.push(Board::new(chunk.to_vec()));
     }
-    println!("t: {:#?}", boards);
-    println!("day3_p1 {}", part1());
-    println!("day3_p2 {}", part2());
+
+    println!("Part1: {}", part1(&mut boards, &nums));
+    println!("Part2: {}", part2(&mut boards, &nums));
 }
