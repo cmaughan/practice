6bc413a97d44a4de7d0a40f8e4e15c44021f517f
Chris Maughan <mornymorny@gmail.com>
2021-12-07 22:07:08 +0000

    Updates

--- rust/advent_of_code/aoc2021/day5/src/main.rs
+++ rust/advent_of_code/aoc2021/day5/src/main.rs
@@ -1,8 +1,6 @@
-use lazy_static::lazy_static;
 use std::collections::hash_map::HashMap;
-use regex::Regex;
 
-fn solve(vals: &Vec<(i32, i32, i32, i32)>, diag: bool) -> usize {
+fn solve(vals: &[(i32, i32, i32, i32)], diag: bool) -> usize {
     let mut m = HashMap::new();
     for (x1, y1, x2, y2) in vals {
         if !diag && !(x1 == x2 || y1 == y2) {
@@ -23,26 +21,23 @@ fn solve(vals: &Vec<(i32, i32, i32, i32)>, diag: bool) -> usize {
     m.values().filter(|&&v| v > 1).count()
 }
 
+/*
 lazy_static! {
     static ref RE : Regex = Regex::new(r"(\d+),(\d+) -> (\d+),(\d+)").unwrap();
 }
+*/
 pub fn main() {
-    let input = include_str!("../input.txt");
-
-     let vals = RE.captures_iter(input)
-         .map(|x| (x[1].parse().unwrap(), x[2].parse().unwrap(), x[3].parse().unwrap(), x[4].parse()
-             .unwrap()))
-         .collect();
-    /*let vals = input.split_ascii_whitespace()
-        .filter(|l| *l != "->")
-        .map(|s| s.split(','))
+    let vals = include_str!("../input.txt")
+        .lines()
+        .map(|l| l.split(" -> "))
+        .flatten()
+        .map(|l| l.split(','))
         .flatten()
         .map(|i| i.parse::<i32>().unwrap())
         .collect::<Vec<_>>()
         .chunks(4)
         .map(|v| (v[0], v[1], v[2], v[3]))
-        .collect();
-     */
+        .collect::<Vec<_>>();
 
     println!("Part1: {}", solve(&vals, false));
     println!("Part2: {}", solve(&vals, true));
--- rust/advent_of_code/aoc2021/day7/src/main.rs
+++ rust/advent_of_code/aoc2021/day7/src/main.rs
@@ -1,27 +1,25 @@
+use itertools::Itertools;
+
 fn part1(vals : &[i64]) -> i64 {
-    let align = vals.iter().sum::<i64>() / vals.len() as i64;
-    (0..align + 1)
-        .map(|c| vals.iter().map(|&v| (v - c).abs()).sum())
-        .min()
-        .unwrap()
+    let c = vals[vals.len() / 2];
+    vals.iter().map(|&v| (v - c).abs()).sum()
 }
 
 fn part2(vals : &[i64]) -> i64 {
     let align = vals.iter().sum::<i64>() / vals.len() as i64;
-    (0..align + 1)
-        .map(|c| vals.iter().map(|&v| {
+    (align-1..align+1).map(|c| {
+        vals.iter().map(|&v| {
             let d = (v-c).abs();
             (d * (d + 1)) / 2
-        }).sum())
-        .min()
-        .unwrap()
+        }).sum()}).min().unwrap()
 }
 
 pub fn main() {
     let input = include_str!("../input.txt")
         .trim()
         .split(',')
-        .map(|v| v.parse::<i64>().unwrap())
+        .map(|v| v.parse().unwrap())
+        .sorted()
         .collect::<Vec<_>>();
 
     println!("Part1: {}", part1(&input));
