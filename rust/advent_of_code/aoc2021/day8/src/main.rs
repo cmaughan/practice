ad02c67c5df766628b3cb56c3f8fd4ab6da6d1e8
Chris Maughan <mornymorny@gmail.com>
2021-12-09 10:19:34 +0000

    Update

--- rust/advent_of_code/aoc2021/day8/src/main.rs
+++ rust/advent_of_code/aoc2021/day8/src/main.rs
@@ -1,121 +1,59 @@
-use itertools::Itertools;
 use std::collections::HashMap;
 
-#[derive(Debug)]
-struct Row {
-    inputs: Vec<String>,
-    outputs: Vec<String>
+fn part1((_,b): &(Vec<&str>, Vec<&str>)) -> usize {
+    b.iter().filter(|x| [2,3,4,7].contains(&x.len())).count()
 }
 
-impl Row {
-    pub fn new(ins: Vec<String>, outs: Vec<String>) -> Row {
-        Row {
-            inputs: ins,
-            outputs: outs
-        }
-    }
-}
-pub fn common_chars(a: &str, b:&str) -> usize {
-    a.chars().fold(0, |a, c| if b.chars().contains(&c) {
-        a + 1
-    } else {
-        a
-    })
+fn part2(rows: &Vec<(Vec<&str>, Vec<&str>)>) -> i32 {
+    rows.iter().map(|(ins, outs)| {
+        // Match the known
+        let h = ins.iter().fold(HashMap::new(), |mut h, s| {
+            match s.len() {
+                2 => h.insert(1, s),
+                3 => h.insert(3, s),
+                4 => h.insert(4, s),
+                7 => h.insert(8, s),
+                _ => None
+            };
+            h
+        });
+
+        // Walk the outputs and find their values based on what we know
+        let res = outs.iter().map(|&s| {
+            let common_one = h[&1].chars().fold(0, |acc, c| if s.contains(c) { acc + 1 }
+            else { acc });
+            let common_four = h[&4].chars().fold(0, |acc, c| if s.contains(c) { acc + 1 }
+            else { acc });
+
+            match (s.len(), common_one, common_four) {
+                (2,2,2) => 1,
+                (5,1,2) => 2,
+                (5,2,3) => 3,
+                (4,2,4) => 4,
+                (5,1,3) => 5,
+                (6,1,3) => 6,
+                (3,2,2) => 7,
+                (7,2,4) => 8,
+                (6,2,4) => 9,
+                (6,2,3) => 0,
+                (_,_,_) => 0
+            }
+        }).collect::<Vec<i32>>(); // row result
+        res[0] * 1000 + res[1] * 100 + res[2] * 10 + res[3]
+    }).sum::<i32>() // Final sum
 }
 
 pub fn main() {
-    let input: Vec<&str> = include_str!("../input.txt")
+    let data = include_str!("../input.txt")
         .lines()
-        .collect();
+        .map(|l| {
+            let (inputs, outputs) = l.split_once("|").unwrap();
+            let ins = inputs.split_ascii_whitespace().collect();
+            let outs = outputs.split_ascii_whitespace().collect();
+            (ins, outs)
+        }).collect::<Vec<_>>();
 
-    let mut rows = Vec::new();
-    for s in input {
-        let mut inputs = Vec::new();
-        let mut outputs = Vec::new();
-
-        let vals : Vec<&str> = s.split_ascii_whitespace().collect();
-        let mut ins = true;
-        for v in vals {
-            if v == "|" {
-                ins = false;
-                continue;
-            }
-            let sorted = v.chars().sorted().collect();
-            if ins {
-                inputs.push(sorted);
-            } else {
-                outputs.push(sorted);
-            }
-        }
-
-        rows.push(Row::new(inputs, outputs));
-    }
-
-    let count = rows.iter().map(|v| v.outputs.iter().filter(|v| match(v.len()) {
-        2 => true, // 1
-        4 => true, // 4
-        3 => true, // 7
-        7 => true,
-        _ => false
-    }).count()).sum::<usize>();
-
-    println!("Part1: {}", count);
-
-    let mut total = 0;
-    for row in rows.iter() {
-        let mut nums = std::collections::HashMap::new();
-        for v in row.inputs.iter() {
-            nums.insert(match v.len() {
-                // A 1, 4, 7, 8
-                2 => 1,
-                4 => 4,
-                3 => 7,
-                7 => 8,
-                _ => 9
-            }, v);
-        }
-
-        for v in row.inputs.iter() {
-            let common_1 = common_chars(v, nums.get(&1).unwrap());
-            let common_4 = common_chars(v, nums.get(&4).unwrap());
-            let common_7 = common_chars(v, nums.get(&7).unwrap());
-            let common_8 = common_chars(v, nums.get(&8).unwrap());
-            if v.len() == 6 {
-                if common_1 == 1 {
-                    nums.insert(6,  v); // 6
-                } else if common_4 == 3 {
-                    nums.insert(0, v); // 0
-                } else if common_1 == 2 && common_4 == 4 {
-                    nums.insert(9, v); // 9
-                }
-                else {
-                    panic!();
-                }
-            } else if v.len() == 5 {
-                if common_8 == 5 && common_1 == 1 && common_4 == 2 {
-                    nums.insert(2, v); // 2
-                } else if common_8 == 5 && common_1 == 2 {
-                    nums.insert(3, v); // 3
-                } else if common_7 == 2 && common_1 == 1 {
-                    nums.insert(5, v); // 5
-                }
-                else {
-                    panic!();
-                }
-            }
-        }
-
-        let mut s: HashMap<String, i32> = HashMap::new();
-        for (k, v) in nums.iter() {
-            s.insert(v.to_string(), *k);
-        }
-
-        let mut mul = 1000;
-        for v in row.outputs.iter() {
-            total += s.get(v.as_str()).unwrap() * mul;
-            mul /= 10;
-        }
-    }
-
-    println!("Part2: {}", total);
+    println!("Part1: {}", data.iter().map(part1).sum::<usize>());
+    println!("Part2: {}", part2(&data));
 }
+
