584e3dee2b60d2a12b35e9fb73b6e1c50561c343
cmaughan <mornymorny@gmail.com>
2021-12-28 18:43:20 +0000

    u8

--- rust/advent_of_code/aoc2021/day15/src/main.rs
+++ rust/advent_of_code/aoc2021/day15/src/main.rs
@@ -1,7 +1,7 @@
 use std::{cmp::Reverse, collections::BinaryHeap};
 use ndarray::{Array, Array2};
 
-fn part1(grid : &Array2<usize>) -> usize {
+fn part1(grid : &Array2<u8>) -> usize {
     let mut q = BinaryHeap::new();
     let mut distances : Array2<usize> = Array::from_elem(grid.raw_dim(), usize::MAX);
     q.push((Reverse(0), (0, 0)));
@@ -23,10 +23,10 @@ fn part1(grid : &Array2<usize>) -> usize {
                 continue;
             }
             let new_pos = (new_x as usize, new_y as usize);
-            let cave = grid[new_pos];
-            if (cost + cave) < distances[new_pos] {
-                q.push((Reverse(cost + cave), new_pos));
-                distances[new_pos] = cost + cave;
+            let cave_cost = grid[new_pos] as usize + cost;
+            if cave_cost < distances[new_pos] {
+                q.push((Reverse(cave_cost), new_pos));
+                distances[new_pos] = cave_cost;
             }
         }
     }
@@ -40,8 +40,8 @@ pub fn main() {
 
     let vals = input.bytes()
         .filter(|&c| c != b'\n')
-        .map(|v| (v - b'0') as usize)
-        .collect::<Vec<usize>>();
+        .map(|v| (v - b'0') as u8)
+        .collect::<Vec<u8>>();
 
     let a = Array::from_shape_fn((width, height), |(x, y)| vals[y * width + x]);
 
@@ -49,7 +49,7 @@ pub fn main() {
     let b = Array::from_shape_fn((width * 5, height * 5), |(x, y)| {
         let (orig_x, orig_y) = (x % width, y % height);
         let (x_tile, y_tile) = (x / width, y / height);
-        ((vals[orig_y * width + orig_x] + x_tile + y_tile - 1) % 9) + 1
+        ((vals[orig_y * width + orig_x] + x_tile as u8 + y_tile as u8 - 1) % 9) + 1
     });
 
     println!("Part1: {}", part1(&a));
