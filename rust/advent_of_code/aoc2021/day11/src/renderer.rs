5d7747fa485b8ad96ff1286a9378b8e04eba182a
Chris Maughan <mornymorny@gmail.com>
2021-12-29 12:57:30 +0000

    renderer

--- rust/advent_of_code/aoc2021/day11/src/renderer.rs
+++ rust/advent_of_code/aoc2021/day11/src/renderer.rs
@@ -1,40 +1,72 @@
 use nannou::prelude::*;
-use std::cell::RefCell;
-use std::sync::Arc;
+use std::sync::{Mutex};
+use lazy_static::lazy_static;
+use std::thread::sleep;
+use std::time::Duration;
 
-static mut Data : Option<&'static mut Vec<Vec<u8>>> = None;
-
-pub fn render(data: &mut Vec<Vec<u8>>) {
-    Data = Some(data);
-    nannou::sketch(view).size(800, 00).run();
+lazy_static! {
+    static ref GRID : Mutex<Vec<Vec<u8>>> = Mutex::new(Vec::new());
 }
 
-fn view(app: &App, frame: Frame) {
+pub fn render(data: &mut Vec<Vec<u8>>) {
+    for row in data.iter() {
+        GRID.lock().unwrap().push(row.clone());
+    }
+    nannou::app(model).update(update).simple_window(view).run();
+}
+struct Model {}
+
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {
+}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let mut data = GRID.lock().unwrap();
+    crate::step(&mut data);
 
     let draw = app.draw();
     draw.background().color(BLACK);
 
-    let min_hue_value = 1.0;
-    let max_hue_value = 0.98;
-    let _win = app.window_rect();
+    let win = app.window_rect();
 
-    let hue = map_range(
-        0.5,
-        0.0,
-        1.0,
-        min_hue_value,
-        max_hue_value,
-    );
-    let c = hsva(
-        hue,
-        0.5,
-        1.0,
-        1.0 - 0.5,
-    );
-    draw.line()
-        .start(Vec2::new(0.0, 0.0))
-        .end(Vec2::new(10.0, 10.0))
-        .stroke_weight(1.0)
-        .color(c);
+    let block = win.pad(25.0).h() / (data[0].len() + 1) as f32;
+    let origin = win.pad(25.0).top_left() + Point2::new(block, -block);
+    for y in 0..data.len() {
+        for x in 0..data[0].len(){
+            let (x, y) = (x as f32, y as f32);
+            let r= Rect::from_x_y_w_h(origin.x + x*block, origin.y - y * block, block, block);
+
+            let hue ;
+            let val ;
+            let mut sat = data[y as usize][x as usize] as f32 ;
+            if sat == 0.0 {
+                sat = 1.0;
+                hue = 0.0;
+                val = 1.0;
+            }
+            else {
+                sat = 1.0 - (sat / 9.0);
+                hue = 117.0 / 255.0;
+                val = sat / 9.0;
+            }
+
+            let c = hsva(
+                hue,
+                sat,
+                val,
+                1.0 - 0.5,
+            );
+
+            draw.rect()
+                .xy(r.xy())
+                .wh(r.wh())
+                .stroke_weight(1.0)
+                .color(c);
+        }
+    }
     draw.to_frame(app, &frame).unwrap();
+    sleep(Duration::new(0, 10000000));
 }
