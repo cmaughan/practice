3e085ae170379dc558455d8bbd7dbe68d5a25c64
Morny <mornymorny@gmail.com>
2022-12-06 09:06:21 +0000

    6

--- cpp/advent_of_code/2022/day_6.cpp
+++ cpp/advent_of_code/2022/day_6.cpp
@@ -1,7 +1,7 @@
 #include "common.h"
 
-#include <deque>
 #include <algorithm>
+#include <deque>
 #include <mutils/time/profiler.h>
 #include <numeric>
 #include <unordered_set>
@@ -15,68 +15,29 @@ using namespace MUtils;
 
 namespace
 {
-std::string part1;
-std::string part2;
+uint32_t part1;
+uint32_t part2;
 } // namespace
 
-class AOC_2022_Day5 : public Object
+class AOC_2022_Day6 : public Object
 {
     virtual void RunOnce() override
     {
         PROFILE_SCOPE(Solution);
-        auto rows = string_split_delim_string(file_read(PRACTICE_ROOT "/advent_of_code/2022/inputs/day_5.txt"), "\n\n");
-        auto crates = string_split_lines(string_split(rows[0], "1")[0]);
-        std::reverse(crates.begin(), crates.end());
-
-        std::vector<std::deque<char>> stacks[2];
-
-        stacks[0].resize(9);
-        for (auto crate_row : crates)
-        {
-            int stack = 0;
-            for (int index = 1; index < crate_row.length(); index += 4)
+        auto input = file_read(PRACTICE_ROOT "/advent_of_code/2022/inputs/day_6.txt");
+        auto findMarkerCount = [&](auto count) {
+            for (int i = 0; i < input.size(); i++)
             {
-                if (crate_row[index] != ' ')
+                if (std::unordered_set<char>{ &input[i], &input[i + count] }.size() == count)
                 {
-                    stacks[0][stack].push_back(crate_row[index]);
+                    return i + count;
                 }
-                stack++;
             }
-        }
-        stacks[1] = stacks[0];
+            return 0;
+        };
 
-        auto commands = string_split_lines(rows[1]);
-        for (auto& command : commands) 
-        {
-            int count, from, to;
-            sscanf(command.c_str(), "move %i from %i to %i", &count, &from, &to);
-            from--;
-            to--;
-            for (int i = 0; i < count; i++)
-            {
-                stacks[0][to].push_back(stacks[0][from].back());
-                stacks[0][from].pop_back();
-            }
-
-            auto end = stacks[1][from].end();
-            auto start = end - count;
-            for (auto itr = start; itr != end; itr++)
-            {
-                stacks[1][to].push_back(*itr);
-            }
-            stacks[1][from].erase(start, end);
-        }
-
-        for (auto& stack : stacks[0])
-        {
-            part1 += stack.back();
-        }
-        
-        for (auto& stack : stacks[1])
-        {
-            part2 += stack.back();
-        }
-
+        part1 = findMarkerCount(4);
+        part2 = findMarkerCount(14);
     }
 
     virtual void DrawGUI() override
@@ -91,4 +52,4 @@ class AOC_2022_Day5 : public Object
     }
 };
 
-REGISTER_PROBLEM(AOC_2022_Day5);
+REGISTER_PROBLEM(AOC_2022_Day6);
